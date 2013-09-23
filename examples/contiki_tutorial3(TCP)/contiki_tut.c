cb995165ac2337cc5bf44c83615ff2077263a9a9
Sven Zehl <svenzehl@web.de>
2013-09-23 15:21:26 +0200

    changed TCP Server hence it fits better on the slides :-)

--- examples/contiki_tutorial3(TCP)/contiki_tut.c
+++ examples/contiki_tutorial3(TCP)/contiki_tut.c
@@ -14,164 +14,49 @@ PROCESS(switch_led_on, "Turns on the LED when device is powered on");
 
 AUTOSTART_PROCESSES(&switch_led_on, &example_psock_server_process);
 
-/*
-* This is a small example of how to write a TCP server using
-* Contiki's protosockets. It is a simple server that accepts one line
-* of text from the TCP connection, and echoes back the first 10 bytes
-* of the string, and then closes the connection.
-*
-* The server only handles one connection at a time.
-*
-*/
-
-
-/*
-* We define one protosocket since we've decided to only handle one
-* connection at a time. If we want to be able to handle more than one
-* connection at a time, each parallell connection needs its own
-* protosocket.
-*/
 static struct psock ps;
 
-/*
-* We must have somewhere to put incoming data, and we use a 10 byte
-* buffer for this purpose.
-*/
 static char buffer[36];
 
-/*---------------------------------------------------------------------------*/
-/*
-* A protosocket always requires a protothread. The protothread
-* contains the code that uses the protosocket. We define the
-* protothread here.
-*/
-static
-PT_THREAD(handle_connection(struct psock *p))
-{
-  /*
-* A protosocket's protothread must start with a PSOCK_BEGIN(), with
-* the protosocket as argument.
-*
-* Remember that the same rules as for protothreads apply: do NOT
-* use local variables unless you are very sure what you are doing!
-* Local (stack) variables are not preserved when the protothread
-* blocks.
-*/
-  PSOCK_BEGIN(p);
 
-  /*
-* We start by sending out a welcoming message. The message is sent
-* using the PSOCK_SEND_STR() function that sends a null-terminated
-* string.
-*/
+static PT_THREAD(handle_connection(struct psock *p))
+{
+  PSOCK_BEGIN(p);
   PSOCK_SEND_STR(p, "Welcome, your request will be processed...\n");
-  
-  /*
-* Next, we use the PSOCK_READTO() function to read incoming data
-* from the TCP connection until we get a newline character. The
-* number of bytes that we actually keep is dependant of the length
-* of the input buffer that we use. Since we only have a 10 byte
-* buffer here (the buffer[] array), we can only remember the first
-* 10 bytes received. The rest of the line up to the newline simply
-* is discarded.
-*/
   PSOCK_READTO(p, '\n');
-  
-  /*
-* And we send back the contents of the buffer. The PSOCK_DATALEN()
-* function provides us with the length of the data that we've
-* received. Note that this length will not be longer than the input
-* buffer we're using.
-*/
   printf("TCP Server received: %s \n",buffer);
   PSOCK_SEND_STR(p, "You wrote: ");
   PSOCK_SEND(p, buffer, PSOCK_DATALEN(p));
   PSOCK_SEND_STR(p, "Good bye!\r\n");
-
-  /*
-* We close the protosocket.
-*/
   PSOCK_CLOSE(p);
-
-  /*
-* And end the protosocket's protothread.
-*/
   PSOCK_END(p);
 }
 /*---------------------------------------------------------------------------*/
 
 
-
-/*
-* The definition of the process.
-*/
 PROCESS_THREAD(example_psock_server_process, ev, data)
 {
-  /*
-* The process begins here.
-*/
-  PROCESS_BEGIN();
-
-  /*
-* We start with setting up a listening TCP port. Note how we're
-* using the UIP_HTONS() macro to convert the port number (1010) to
-* network byte order as required by the tcp_listen() function.
-*/
-  tcp_listen(UIP_HTONS(1010));
-
-  /*
-* We loop for ever, accepting new connections.
-*/
-  while(1) {
-
-    /*
-* We wait until we get the first TCP/IP event, which probably
-* comes because someone connected to us.
-*/
-    PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
-
-    /*
-* If a peer connected with us, we'll initialize the protosocket
-* with PSOCK_INIT().
-*/
-    if(uip_connected()) {
-      
-      /*
-* The PSOCK_INIT() function initializes the protosocket and
-* binds the input buffer to the protosocket.
-*/
-      PSOCK_INIT(&ps, buffer, sizeof(buffer));
-
-      /*
-* We loop until the connection is aborted, closed, or times out.
-*/
-      while(!(uip_aborted() || uip_closed() || uip_timedout())) {
-
-/*
-* We wait until we get a TCP/IP event. Remember that we
-* always need to wait for events inside a process, to let
-* other processes run while we are waiting.
-*/
-PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
-
-/*
-* Here is where the real work is taking place: we call the
-* handle_connection() protothread that we defined above. This
-* protothread uses the protosocket to receive the data that
-* we want it to.
-*/
-handle_connection(&ps);
-      }
-    }
-  }
-  
-  /*
-* We must always declare the end of a process.
-*/
-  PROCESS_END();
+ PROCESS_BEGIN();
+ /*Festlegung des Listening Ports*/
+ tcp_listen(UIP_HTONS(1010));
+ while(1) 
+ {
+  printf("TCP Server is running\n");
+  /*Warte bis ein Packet eingetroffen ist*/
+  PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
+   /*Bei Verbindungsanfrage initialisiere Protosocket*/
+   if(uip_connected()) 
+   {
+    PSOCK_INIT(&ps, buffer, sizeof(buffer));
+    while(!(uip_aborted()||uip_closed()||uip_timedout()))
+    {
+     PROCESS_WAIT_EVENT_UNTIL(ev == tcpip_event);
+      handle_connection(&ps);
+    }/*Aufruf der Handler-Funktion*/
+   }
+ }
+ PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
-
 
 
 PROCESS_THREAD(switch_led_on, ev, data)
